3ecddf25e6d7c46da2e969acb12155f18657d5b3
Tanvir Ibn Touhid <tanvirbintouhid5230@gmail.com>
2020-09-18 23:01:32 +0600

    firebase auth added

--- src/components/BookingDetails/BookingDetails.js
+++ src/components/BookingDetails/BookingDetails.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { Col, Container, Form, FormGroup, Input, Label, Row } from "reactstrap";
 import { data } from "../../resources/data";
@@ -6,8 +6,27 @@ import MyNavbar from "../navbar/Navbar";
 
 const BookingDetails = () => {
   const { id } = useParams();
-
   const place = data.find((item) => item.id === parseInt(id));
+
+  const [bookingDetails, setBookingDetails] = useState({
+    origin: "Dhaka",
+    destination: place.name,
+    dateFrom: "",
+    dateTo: "",
+  });
+  const handleBlur = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    const newBooking = {
+      ...bookingDetails,
+      [name]: value,
+    };
+    setBookingDetails(newBooking);
+  };
+  const handleSubmit = (e) => {
+    alert(JSON.stringify(bookingDetails));
+    e.preventDefault();
+  };
   return (
     <div className="bg-overlay">
       <Container>
@@ -26,10 +45,15 @@ const BookingDetails = () => {
           </Col>
           <Col className="m-auto py-5" sm="12" md="8" lg="5">
             <div className="booking-div bg-white rounded-lg">
-              <Form className="p-4 ">
+              <Form className="p-4">
                 <FormGroup>
                   <Label for="origin">Origin</Label>
-                  <Input className="form-group" type="select" name="select">
+                  <Input
+                    className="form-group"
+                    type="select"
+                    name="origin"
+                    onBlur={handleBlur}
+                  >
                     <option>Dhaka</option>
                     <option>Cumilla</option>
                     <option>Sylhet</option>
@@ -37,17 +61,34 @@ const BookingDetails = () => {
                 </FormGroup>
                 <FormGroup>
                   <Label for="destination">Destination</Label>
-                  <Input type="text" value={place.name} readOnly></Input>
+                  <Input
+                    type="text"
+                    name="destination"
+                    value={place.name}
+                    readOnly
+                  ></Input>
                 </FormGroup>
                 <FormGroup>
                   <Row>
                     <Col md="6" className="mb-3 mb-md-0">
                       <Label for="from">From</Label>
-                      <Input type="date" id="from" name="from"></Input>
+                      <Input
+                        type="date"
+                        id="from"
+                        name="dateFrom"
+                        onBlur={handleBlur}
+                        required="required"
+                      ></Input>
                     </Col>
                     <Col md="6">
                       <Label for="To">To</Label>
-                      <Input type="date" id="to" name="to"></Input>
+                      <Input
+                        type="date"
+                        id="to"
+                        name="dateTo"
+                        onBlur={handleBlur}
+                        required="required"
+                      ></Input>
                     </Col>
                   </Row>
                 </FormGroup>
@@ -55,6 +96,7 @@ const BookingDetails = () => {
                   className="d-block w-100 mt-3 btn btn-warning"
                   type="submit"
                   value="Start Booking"
+                  onClick={handleSubmit}
                 />
               </Form>
             </div>
---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB0V11dPHfQunqRP0gJCF7-yUR0RXuRVvA",
+  authDomain: "travel-guru-t.firebaseapp.com",
+  databaseURL: "https://travel-guru-t.firebaseio.com",
+  projectId: "travel-guru-t",
+  storageBucket: "travel-guru-t.appspot.com",
+  messagingSenderId: "811036991541",
+  appId: "1:811036991541:web:e10306731848fb7f62117c",
+};
